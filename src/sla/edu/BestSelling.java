72aa87aebf8588e46999dd18ddccf8ef4c4cabec
120503tz <zeiglertashon@gmail.com>
2021-01-06 09:12:50 -0500

    redid Best Selling

--- src/sla/edu/BestSelling.java
+++ src/sla/edu/BestSelling.java
@@ -3,13 +3,15 @@ package sla.edu;
 public class BestSelling extends VideoGame {
     //fields
     private int sales;
+    private int rank;
 
     //constructors
 
 
-    public BestSelling(int rank, String title, int releaseYear, String releaseDate, String developer, String publisher, String platform, int sales) {
-        super(rank, title, releaseYear, releaseDate, developer, publisher, platform);
+    public BestSelling(String title, int releaseYear, String releaseDate, String developer, String publisher, String platform, int sales, int rank) {
+        super(title, releaseYear, releaseDate, developer, publisher, platform);
         this.sales = sales;
+        this.rank = rank;
     }
     //setters/getters
 
@@ -19,6 +21,14 @@ public class BestSelling extends VideoGame {
 
     public void setSales(int sales) {
         this.sales = sales;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
     }
 }
 
