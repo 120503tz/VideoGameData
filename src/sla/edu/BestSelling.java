3b68071f9ea71a643805992c7e47307b7ba027d4
120503tz <zeiglertashon@gmail.com>
2021-01-06 10:16:02 -0500

    redid Best Selling again

--- src/sla/edu/BestSelling.java
+++ src/sla/edu/BestSelling.java
@@ -2,31 +2,53 @@ package sla.edu;
 
 public class BestSelling extends VideoGame {
     //fields
-    private String releaseDate;
     private int sales;
     private int rank;
+    private int releaseYear;
+    private int releaseDate;
 
     //constructors
 
 
-    public BestSelling(String title, String releaseDate, String developer, String publisher, String platform, int sales, int rank) {
+    public BestSelling(String title, String developer, String publisher, String platform, int sales, int rank, int releaseYear, int releaseDate) {
         super(title, developer, publisher, platform);
-        this.releaseDate = releaseDate;
         this.sales = sales;
         this.rank = rank;
+        this.releaseYear = releaseYear;
+        this.releaseDate = releaseDate;
     }
     //setters/getters
 
-    public String getReleaseDate() { return releaseDate; }
+    public int getSales() {
+        return sales;
+    }
 
-    public void setReleaseDate(String releaseDate) { this.releaseDate = releaseDate; }
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
 
-    public int getSales() { return sales; }
+    public int getRank() {
+        return rank;
+    }
 
-    public void setSales(int sales) { this.sales = sales; }
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
 
-    public int getRank() { return rank; }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
 
-    public void setRank(int rank) { this.rank = rank; }
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
 
